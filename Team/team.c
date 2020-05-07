589985605c4070e4ea6a1a6f03cb1c15fbcb7830
alastiesas <agustin.lastiesas@gmail.com>
2020-05-07 20:17:44 -0300

    inicializar entrenador y cambiar de estado

--- Team/team.c
+++ Team/team.c
@@ -69,14 +69,24 @@ int main(void)
 	puerto = config_get_string_value(config, "PUERTO");
 
 
-	t_position* test_posicion = construct_position(atoi(test_split_position[0]),atoi(test_split_position[1]));
-	//t_trainer* test_entrenador = malloc(sizeof(t_trainer));
+
+	//ciclo para cargar una lista de entrenadores.
+	//t_trainer* test_entrenador = construct_trainer(test_postions[0], test_objetivos[0], test_pokemons[0]);
+	t_list* new_list = initialize_trainers(test_postions, test_objetivos, test_pokemons);
+	t_list* ready_list = list_create();
+	t_list* block_list = list_create();
+	t_list* exec_list = list_create();
+	t_list* exit_list = list_create();
+	state_change(1,new_list, ready_list);
+	t_trainer* trainer = (t_trainer*) list_get(ready_list,0);
+
+	printf("el debug de los entrenador %s\n", trainer->objectives[2]);
+
+	//FIFO funcion generica que recibe tipo de planificador, "fifo" agarra el primero en la cola
+	//agarra lista ready y se fija cual es el que pasa a lista exec
 
 
-
-
-
-
+	//5 colas para los estados
 
 	/*
 	conexion = crear_conexion(ip, puerto);
--- Team/team.h
+++ Team/team.h
@@ -21,36 +21,71 @@
 typedef struct
 {
 	uint32_t id;
-	t_position position;
+	t_position* position;
 	char** objectives;
 	char** pokemons;
 } t_trainer;
 
 int size_array (char*);
-t_trainer* construct_trainer(*t_position, char**, char**);
-t_position* construct_position(uint32_t, uint32_t);
+t_trainer* construct_trainer(char* positions, char**, char**);
+t_position* construct_position(char*);
+t_list * initialize_trainers(char**,char**,char**);
+void state_change(int index, t_list* from,t_list* to);
 
-int size_array (char* array){
+
+
+
+int size_array (char* array)
+{
 	return char_count(array,'|');
 }
 
 
-
-t_trainer* construct_trainer(t_position* position, char** objectives, char** pokemons){
+t_trainer* construct_trainer(char* positions, char** objectives, char** pokemons)
+{
 	t_trainer* trainer = malloc(sizeof(t_trainer));
-	trainer->position = position;
-	trainer->objectives = objectives;
-	trainer->pokemons = pokemons;
 
+	trainer->position = construct_position(positions);
+	trainer->objectives = string_split(objectives, "|");
+	trainer->pokemons = string_split(pokemons, "|");
+	/*
+	printf("test debug pokemon %d\n",trainer->position->x);
+	printf("test debug pokemon %d\n",trainer->position->y);
+	printf("test debug pokemon %s\n",trainer->objectives[0]);
+	printf("test debug pokemon %s\n",trainer->pokemons[2]);
+	*/
 	return trainer;
 }
 
-t_position* construct_position(uint32_t x , uint32_t y){
+t_position* construct_position(char* positions)
+{
 	t_position* position = malloc(sizeof(t_position));
-	position->x = x;
-	position->y = y;
+	char ** positions_split = string_split(positions, "|");
+	position->x = atoi(positions_split[0]);
+	position->y = atoi(positions_split[1]);
+
 
 	return position;
 }
+
+t_list * initialize_trainers(char** positions_config,char** objectives_config,char** pokemons_config)
+{
+	int k = 0;
+	t_list * trainers_list = list_create();
+	while(positions_config[k]){
+		t_trainer* test_entrenador = construct_trainer(positions_config[k], objectives_config[k], pokemons_config[k]);
+		list_add(trainers_list, test_entrenador);
+		k++;
+	}
+
+	return trainers_list;
+}
+
+void state_change(int index, t_list* from,t_list* to)
+{
+	void* element = list_remove(from, index);
+	list_add(to, element);
+}
+
 
 #endif /* TEAM_H_ */
