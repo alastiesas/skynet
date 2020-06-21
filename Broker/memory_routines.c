2870329113820ddf0e3f4906e80ff8c3fc44d3cd
nehuenesquivel <nehuenesquivel@outlook.com>
2020-06-20 22:12:32 -0300

    I don't want conflicts.

--- Broker/memory_routines.c
+++ Broker/memory_routines.c
@@ -1,5 +1,16 @@
 #include "broker.h"
 
+void create_partition(uint32_t size) {
+
+	if (strcmp(memory_algorithm, "BS") == 0) {
+
+		create_fixed_partition(size);
+	} else if (strcmp(memory_algorithm, "PARTICIONES") == 0) {
+
+		create_dynamic_partition(size);
+	}
+}
+
 void create_dynamic_partition(uint32_t size) {
 
 	t_partition* partition = malloc(sizeof(t_partition));
@@ -21,22 +32,40 @@ void create_fixed_partition(uint32_t size) {
 
 }
 
-void create_partition(uint32_t size) {
+void delete_partition() {
 
-	if (strcmp(memory_algorithm, "BS") == 0) {
-
-		create_fixed_partition(size);
-	} else if (strcmp(memory_algorithm, "PARTICIONES") == 0) {
-
-		create_dynamic_partition(size);
-	}
 }
 
 void delete_dynamic_partition() {
 
-//	t_partition* victim_partition = ;
+	uint32_t victim_partition_number = get_partition_number_to_delete();
+	uint32_t previous_partition_number = victim_partition_number - 1;
+	uint32_t next_partition_number = victim_partition_number + 1;
+	t_partition* victim_partition = list_get(partitions, victim_partition_number);
 
-//	victim_partition->available = true;
+	victim_partition->available = true;
+
+	if (next_partition_number < list_size(partitions)) {
+
+		t_partition* next_partition = list_get(partitions, next_partition_number);
+		if (next_partition->available) {
+
+			merge_partitions(victim_partition_number, next_partition_number);
+		}
+	}
+
+	if (previous_partition_number > -1) {
+
+		t_partition* previous_partition = list_get(partitions, previous_partition_number);
+		if (previous_partition->available) {
+
+			merge_partitions(previous_partition_number, victim_partition_number);
+		}
+	}
+}
+
+void delete_fixed_partition() {
+
 }
 
 t_partition* find_available_dynamic_partition(uint32_t size) {
@@ -70,42 +99,14 @@ t_partition* find_available_dynamic_partition(uint32_t size) {
 	return list_get(partitions, available_partition_number);
 }
 
-void free_partition(){
+void merge_partitions(uint32_t initial_partition_number, uint32_t final_partition_number) {
 
-	uint32_t index_partition = get_partition_number_to_delete();
+	t_partition* initial_partition = list_get(partitions, initial_partition_number);
+	t_partition* final_partition = list_get(partitions, final_partition_number);
 
-	t_partition* partition_to_free = list_get(partitions, index_partition);
-
-	partition_to_free->available = true;
-
-	uint32_t index_ant = index_partition - 1;
-	uint32_t index_prox = index_partition + 1;
-
-	if (index_prox < list_size(partitions)) {
-		t_partition* partition_prox = list_get(partitions, index_prox);
-		if (partition_prox->available == true) {
-
-			//TODO unifico a derecha
-			partition_to_free->final_position = partition_prox->final_position;
-			partition_to_free->size += partition_prox->size;
-			list_remove(partitions, index_prox);
-
-		}
-	}
-
-	if (index_ant >= 0) {
-		t_partition* partition_ant = list_get(partitions, index_ant);
-		if (partition_ant->available == true) {
-
-			//TODO unifico a izquierda
-			partition_to_free->initial_position = partition_ant->initial_position;
-			partition_to_free->size += partition_ant->size;
-			list_remove(partitions, index_ant);
-
-		}
-	}
-
-
+	initial_partition->final_position = final_partition->final_position;
+	initial_partition->size += final_partition->size;
+	list_remove(partitions, final_partition_number); // change
 }
 
 uint32_t get_available_partition_number(uint32_t size) {
@@ -120,9 +121,7 @@ uint32_t get_available_partition_number(uint32_t size) {
 		for (int i = 0; remaining_size > 0 && i < list_size(partitions); i++) {
 
 			partition = list_get(partitions, i);
-			if (partition->available == true
-					&& partition->size - size < remaining_size
-					&& partition->size >= size) {
+			if (partition->available && partition->size - size < remaining_size && partition->size >= size) {
 
 				partition_number = i;
 				remaining_size = partition->size - size;
@@ -130,11 +129,10 @@ uint32_t get_available_partition_number(uint32_t size) {
 		}
 	} else if (strcmp(free_partition_algorithm, "FF") == 0) {
 
-		for (int i = 0; partition_number == -1 && i < list_size(partitions);
-				i++) {
+		for (int i = 0; partition_number == -1 && i < list_size(partitions); i++) {
 
 			partition = list_get(partitions, i);
-			if (partition->available == true && partition->size >= size) {
+			if (partition->available && partition->size >= size) {
 
 				partition_number = i;
 			}
@@ -167,8 +165,7 @@ uint32_t get_partition_number_to_delete() {
 		for (int i = 0; i < list_size(partitions); i++) {
 
 			partition = list_get(partitions, i);
-			if (partition->available == false
-					&& partition->lru > max_lru) {
+			if (partition->available == false && partition->lru > max_lru) {
 
 				partition_number = i;
 				max_lru = partition->lru;
@@ -182,13 +179,9 @@ uint32_t get_partition_number_to_delete() {
 void memory_allocation() {
 
 	mem = malloc(memory_size);
-//	create_dynamic_partition(memory_size);
+	create_dynamic_partition(memory_size);
 }
 
 void memory_compaction() {
-
-}
-
-void merge_partitions() {
 
 }
