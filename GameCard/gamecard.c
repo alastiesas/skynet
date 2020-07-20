ca8edfcb17296f977ee619b323dc383ec35c2100
nehuenesquivel <nehuenesquivel@outlook.com>
2020-07-20 19:10:30 -0300

    testing

--- GameCard/gamecard.c
+++ GameCard/gamecard.c
@@ -46,6 +46,46 @@ int main(void) {
 	config_init();
 
 	init_fs();
+
+	t_message_new* message_new_1 = create_message_new_long("example1", 10, 10,
+			15);
+	new_pokemon_routine(message_new_1);
+
+	t_message_new* message_new_2 = create_message_new_long("example1", 10, 10,
+			15);
+	new_pokemon_routine(message_new_2);
+
+	t_message_new* message_new_3 = create_message_new_long("example2", 10, 10,
+				15);
+		new_pokemon_routine(message_new_3);
+
+	t_message_new* message_new_4 = create_message_new_long("example3", 15, 20,
+			30);
+	new_pokemon_routine(message_new_4);
+
+	t_message_new* message_new_5 = create_message_new_long("example2", 15, 84,
+			2);
+	new_pokemon_routine(message_new_5);
+	t_message_new* message_new_1 = create_message_new_long("example1", 10, 7,
+				15);
+		new_pokemon_routine(message_new_1);
+
+		t_message_new* message_new_2 = create_message_new_long("example1", 10, 10,
+				15);
+		new_pokemon_routine(message_new_2);
+
+		t_message_new* message_new_3 = create_message_new_long("example2", 8, 95,
+					15);
+			new_pokemon_routine(message_new_3);
+
+		t_message_new* message_new_4 = create_message_new_long("example3", 73, 20,
+				30);
+		new_pokemon_routine(message_new_4);
+
+		t_message_new* message_new_5 = create_message_new_long("example2", 68, 84,
+				2);
+		new_pokemon_routine(message_new_5);
+
 	// initiliaze_file_system();
 	t_location* location = malloc(sizeof(t_location));
 	location->position = malloc(sizeof(t_position));
